6b4b9501f23e8b2326d9ab3d02c653e01a20b596
Nuru <nuruzzamannuru7595@gmail.com>
2024-09-28 18:01:07 +0600

    Review Section

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("ri-close-large-line");
 });
 
-navLink.forEach(link => {
+navLink.forEach((link) => {
     link.addEventListener("click", () => {
         navMenu.classList.toggle("left-[0]");
         navMenu.classList.toggle("left-[-100%]");
@@ -19,14 +19,37 @@ navLink.forEach(link => {
 
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
 
-
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
 
-
 /*~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~*/
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    speed: 400,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
 
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    grabCursor: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
 
 /*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
-
 
 /*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
