5c9d48f890b652ca773d17002e9fd810694cce4c
Nuru <nuruzzamannuru7595@gmail.com>
2024-09-28 20:35:59 +0600

    Before last step

--- main.js
+++ main.js
@@ -18,8 +18,32 @@ navLink.forEach((link) => {
 });
 
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById("scroll-up");
+
+    if (this.scrollY >= 250) {
+        scrollUpBtn.classList.remove("-bottom-1/2");
+        scrollUpBtn.classList.add("bottom-4");
+    } else {
+        scrollUpBtn.classList.add("-bottom-1/2");
+        scrollUpBtn.classList.remove("bottom-4");
+    }
+};
+
+window.addEventListener("scroll", scrollUp);
 
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+const scrollHeader = () => {
+    const header = document.getElementById("navbar");
+
+    if (this.scrollY >= 50) {
+        header.classList.add("border-b", "border-yellow-500");
+    } else {
+        header.classList.remove("border-b", "border-yellow-500");
+    }
+};
+
+window.addEventListener("scroll", scrollHeader);
 
 /*~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~*/
 const swiper = new Swiper(".swiper", {
@@ -28,28 +52,51 @@ const swiper = new Swiper(".swiper", {
     spaceBetween: 30,
     autoplay: {
         delay: 3000,
-        disableOnInteraction: false
+        disableOnInteraction: false,
     },
 
     // If we need pagination
     pagination: {
         el: ".swiper-pagination",
-        clickable: true
+        clickable: true,
     },
     grabCursor: true,
     breakpoints: {
         640: {
-            slidesPerView: 1
+            slidesPerView: 1,
         },
         768: {
-            slidesPerView: 2
+            slidesPerView: 2,
         },
         1024: {
-            slidesPerView: 3
-        }
-    }
+            slidesPerView: 3,
+        },
+    },
 });
 
 /*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+const activeLink = () => {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    let current = "home";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+
+        if (this.scrollY >= sectionTop - 60) {
+            current = section.getAttribute("id");
+        }
+    })
+
+    navLinks.forEach(item => {
+        item.classList.remove("active")
+
+        if(item.href.includes(current)){
+            item.classList.add("active")
+        }
+    })
+};
+window.addEventListener("scroll", activeLink);
 
 /*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
